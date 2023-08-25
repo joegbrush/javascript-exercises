9d2b473139839c46bb4d8f6a9f50bf3c95e5e22d
Joe Brush <joegbrush@gmail.com>
2023-08-25 09:50:19 -0500

    Palindromes exercise completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  return string.split("").reverse().join("") === string;
 };
+
+console.log(palindromes("race car"));
 
 // Do not edit below this line
 module.exports = palindromes;
