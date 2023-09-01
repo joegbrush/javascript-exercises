239fa55a4c94ac9b5c1d323b74ec697d872474bf
Joe Brush <joegbrush@gmail.com>
2023-09-01 10:07:32 -0500

    update findTheOldest

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (count) {
+  let firstPrev = 1;
+  let secondPrev = 0;
 
+  for (let i = 2; i < count; i++) {
+    let current = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = current;
+  }
+  return firstPrev;
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
